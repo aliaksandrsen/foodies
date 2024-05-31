17bbcba83b12a1a222375d9c2e0286cd51bab926
Alex <puskin1914@gmail.com>
2024-05-31 17:59:09 +0000

    use Suspense for loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,17 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
+
 import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import { getMeals } from '@/lib/meals';
 
-import Link from 'next/link';
-
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +27,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
